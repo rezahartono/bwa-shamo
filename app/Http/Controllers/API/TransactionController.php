3bdf768a40df7fce1122672bcc5d48ce8d9a7f27
Reza Hartono <rezahartono.nsw@gmail.com>
2021-12-05 09:04:46 +0700

    commit by rezza

---
+++ app/Http/Controllers/API/TransactionController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+
+use App\Helpers\Response;
+use App\Models\Transaction;
+use App\Models\TransactionItem;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class TransactionController extends Controller
+{
+    public function allTransactions(Request $request)
+    {
+        $id = $request->input('id');
+        $limit = $request->input('limit', 6);
+        $status = $request->input('status');
+
+        if ($id) {
+            $transaction = Transaction::with(['items.product']);
+            if ($transaction) {
+                return Response::success(
+                    $transaction,
+                    'Data Transaksi berhasi diambil'
+                );
+            } else {
+                return Response::error(
+                    null,
+                    'Tidak ada transaksi',
+                    404,
+                );
+            }
+        }
+
+        $transaction = Transaction::with(['items.product'])->where('users_id', Auth::user()->id);
+
+        if ($status) {
+            $transaction->where('status', $status);
+        }
+
+        return Response::success(
+            $transaction->paginate($limit),
+            'Data Transaksi diambil'
+        );
+    }
+
+    public function checkoutProduct(Request $request)
+    {
+        $request->validate([
+            'items' => 'required|array',
+            'items.*.id' => 'exists:products,id',
+            'total_price' => 'required',
+            'shipping_price' => 'required',
+            'status' => 'required|in:PENDING,SUCCESS,CANCELLED,FAILED,SHIPPING,SHIPPED',
+        ]);
+
+        $transaction = Transaction::create([
+            'users_id' => Auth::user()->id,
+            'address' => $request->address,
+            'total_price' => $request->total_price,
+            'shipping_price' => $request->shipping_price,
+            'status' => $request->status,
+        ]);
+
+        foreach ($request->items as $product) {
+            TransactionItem::create([
+                'users_id' => Auth::user()->id,
+                'products_id' => $product['id'],
+                'transactions_id' => $transaction->id,
+                'quantity' => $product['quantity'],
+            ]);
+        }
+
+        return Response::success([
+            $transaction->load('items.product'),
+            'Transaksi Berhasil'
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,9 @@
 <?php
 
 use App\Http\Controllers\API\UserController;
-use App\Http\Controllers\ProductCategoryController;
-use App\Http\Controllers\ProductController;
-use App\Http\Controllers\TransactionController;
+use App\Http\Controllers\API\ProductCategoryController;
+use App\Http\Controllers\API\ProductController;
+use App\Http\Controllers\API\TransactionController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
